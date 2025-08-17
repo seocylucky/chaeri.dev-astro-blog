4fcdb06e2472cc271a4a5de02f2d6168d4bcf962
seocylucky <seocylucky@github.com>
2025-08-17 17:56:13 +0900

    fix: a 태그 link 조건문 수정

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,7 +11,12 @@ const Navigation: Component = () => {
       <a href="/about" class={`nav-link ${currentPath === "/about" ? "active" : ""}`}>
         About
       </a>
-      <a href="/posts" class={`nav-link ${currentPath === "/posts" ? "active" : ""}`}>
+      <a
+        href="/posts"
+        class={`nav-link ${
+          currentPath.startsWith("/posts/") || currentPath.startsWith("/tags/") ? "active" : ""
+        }`}
+      >
         Posts
       </a>
       <a href="/guestbook" class={`nav-link ${currentPath === "/guestbook" ? "active" : ""}`}>
