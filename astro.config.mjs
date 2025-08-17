0263b6f88ceaf7d85779ff6eaa8660233e263158
seocylucky <seocylucky@github.com>
2025-08-17 17:29:09 +0900

    feat: tailwind, content, astro config 파일 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 
 import tailwindcss from "@tailwindcss/vite";
+import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://chaeri-dev.netlify.app/",
+  trailingSlash: "ignore",
   integrations: [solid()],
-
+  markdown: {
+    remarkPlugins: [[remarkToc, { heading: "toc", maxDepth: 3 }]],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
