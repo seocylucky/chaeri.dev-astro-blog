1db6c49bc6de450f58e05baf1c4e71c40996c9b9
seocylucky <seocylucky@github.com>
2025-08-27 16:44:55 +0900

    chore: vite 플러그인 제거 및 alias 설정
    - remove invalid vite.plugins setting
    - keep alias @ -> src

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
+import { fileURLToPath } from "url";
 import solid from "@astrojs/solid-js";
-
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
 import remarkToc from "remark-toc";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://chaeri-dev.netlify.app/",
   trailingSlash: "ignore",
-  integrations: [solid()],
+  integrations: [
+    solid(),
+    tailwind(),
+  ],
   markdown: {
     remarkPlugins: [[remarkToc, { heading: "toc", maxDepth: 3 }]],
   },
   vite: {
-    plugins: [tailwindcss()],
+    resolve: {
+      alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },
+    },
   },
 });
