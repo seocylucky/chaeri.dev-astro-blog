f5b3a330b726c53e5c0b6324334eb82192bee766
seocylucky <chaeyeon2285@naver.com>
2025-08-31 22:54:16 +0900

    fix: PostItem 스타일 수정 및 link 경로 변경

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -10,13 +10,13 @@ const PostItem: Component<Props> = (props) => {
   const { post } = props;
   return (
     <a
-      href={`/posts/${post.slug}/`}
-      class="pt-2 pb-2 px-4 flex items-center gap-3 sm:gap-5 rounded-2xl 
+      href={`/posts/${post.slug.split('/')[0]}/`}
+      class="flex items-center gap-3 sm:gap-5 rounded-2xl 
          hover:bg-[#FFD1E7] dark:hover:text-black
          transition-colors duration-150 ease-in-out"
     >
-      <div class="flex items-center justify-center bg-[#FFD1E7] w-[44px] h-[44px] sm:w-[64px] sm:h-[64px] rounded-full">
-        <span class="text-[28px] sm:text-4xl sm:pt-1.5">{post.data.imoji}</span>
+      <div class="flex items-center justify-center bg-[#FFD1E7] w-[44px] h-[44px] sm:w-[60px] sm:h-[60px] rounded-full">
+        <span class="text-[28px] sm:text-4xl sm:pt-0">{post.data.imoji}</span>
       </div>
 
       <li class={`flex items-center w-[82%] sm:[88%] ${props.class} text-[20px] overflow-hidden shrink-0`}>
