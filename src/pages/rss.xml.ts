7d75537da0e58c8251f9d1c0b4f506ae1e50453f
seocylucky <seocylucky@github.com>
2025-11-11 19:19:23 +0900

    feat: readme link

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,20 +4,22 @@ import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ site }) => {
   const all = await getCollection("blog");
+
   const posts: CollectionEntry<"blog">[] = all.sort(
-    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+    (a, b) => new Date(b.data.pubDate).valueOf() - new Date(a.data.pubDate).valueOf()
   );
+
+  const toRoute = (slug: string) => `/posts/${slug.split("/")[0]}/`;
 
   return rss({
     title: "Chae-ri.dev",
     description: "Chae-ri의 개발 블로그",
-    site: site!,
+    site: site ?? new URL("https://chae-ri.dev/"),
     items: posts.map((post) => ({
       title: post.data.title,
-      imoji: post.data.imoji,
-      description: post.data.description,
-      pubDate: post.data.pubDate,
-      link: `/posts/${post.slug}/`,
+      description: post.data.description ?? "",
+      pubDate: new Date(post.data.pubDate),
+      link: toRoute(post.slug),
     })),
     customData: `<language>ko-kr</language>`,
   });
